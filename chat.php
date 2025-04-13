f8d0f6ad3880cc30616a38c0e96fc377ecd78c6e
Masayuki Nii <nii@msyk.net>
2025-04-13 18:50:26 +0900

    Def-file backed.

--- chat.php
+++ chat.php
@@ -30,7 +30,7 @@ IM_Entry(
             'authentication' => array(
                 'all' => array( // load, update, new, delete
                     'target' => 'field-user',
-                    'field' => 'username',
+                    'field' => 'user',
                 ),
             ),
             'post-reconstruct' => true,
